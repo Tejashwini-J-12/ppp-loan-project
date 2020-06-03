c38ea7d62050ddb4dfa412a1a92a70dc5c8bc3a9
Tejashwini-J-12 <62303362+Tejashwini-J-12@users.noreply.github.com>
2020-06-03 14:07:11 +0530

    Add files via upload

---
+++ BusinessInfoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PPP_Loan.Models
+{
+    public class BusinessInfoViewModel
+    {
+        public Address address { get; set; }
+        public BusinessInfo businessInfo { get; set; }
+    }
+}
---
+++ PersonalInfoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PPP_Loan.Models
+{
+    public class PersonalInfoViewModel
+    {
+       
+        public Address address { get;set; }
+        public PersonalInfo personalInfo { get; set; }
+        
+    }
+}

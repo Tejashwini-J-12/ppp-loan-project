3bb01e7a1f96e94044f78be404ce1ba6bfd2d1a1
Tejashwini-J-12 <62303362+Tejashwini-J-12@users.noreply.github.com>
2020-06-11 12:49:29 +0530

    Add files via upload

---
+++ AddressModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PPP_Loan.Models
+{
+    public class AddressModel
+    {
+        [Required]
+        public  string Street { get; set; }
+        [Required]
+        public string City { get; set; }
+        public List<SelectListItem> States { get; set; }
+        public int? StateId { get; set; }
+
+        [Required]
+        [Display(Name ="Zip Code")]
+        public string zipcode { get; set; }
+    }
+}
---
+++ addressController.cs
@@ -0,0 +1,69 @@
+﻿
+using PPP_Loan.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PPP_Loan.Controllers
+{
+    public class addressController : Controller
+    {
+        // GET: address
+        public ActionResult Index()
+        {
+            AddressModel address = new AddressModel();
+            address.States = PopulateStates();
+            return View(address);
+
+        }
+        [HttpPost]
+        public ActionResult Index(AddressModel address)
+        {
+            address.States = PopulateStates();
+            var selectedItem = address.States.Find(p => p.Value == address.StateId.ToString());
+            if (selectedItem != null)
+            {
+                selectedItem.Selected = true;
+                ViewBag.Message = "State: " + selectedItem.Text;
+                //ViewBag.Message += "\\nQuantity: " + fruit.Quantity;
+            }
+
+            return View(address);
+        }
+
+        private static List<SelectListItem> PopulateStates()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            using (SqlConnection con = new SqlConnection("Data Source=DESKTOP-RURNJ5I\\tej;Initial Catalog=Commercial_DB;Integrated Security=True"))
+            //using (SqlConnection con = new SqlConnection(con))
+            {
+                string query = " SELECT state_name, state_id FROM STATES";
+                using (SqlCommand cmd = new SqlCommand(query,con))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            items.Add(new SelectListItem
+                            {
+                                Text = sdr["state_name"].ToString(),
+                                Value = sdr["state_id"].ToString()
+                            });
+                        }
+                    }
+                    con.Close();
+                }
+            }
+
+            return items;
+
+
+        }
+    }
+}
